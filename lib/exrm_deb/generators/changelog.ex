2e2800f2ce9146ac87f997f00f6acbb80f7512c3
Teemu Huhtala <teemu.huhtala@notta.fi>
2017-10-13 16:55:03 +0300

    Updated timex to 3.0

--- lib/exrm_deb/generators/changelog.ex
+++ lib/exrm_deb/generators/changelog.ex
@@ -12,7 +12,7 @@ defmodule ExrmDeb.Generators.Changelog do
     debug "Building Changelog file"
 
     {:ok, time} =
-      Timex.DateTime.now
+      Timex.now
       |> Timex.format("%a, %d %b %Y %H:%M:%S GMT", :strftime)
 
     changelog =
